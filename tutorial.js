6bff6d8ebac3dbc52fb59d032793726c4722abc5
Eddie Sholl <esholl@zendesk.com>
2018-01-21 13:09:36 +1100

    Adds a basic action button and callback URL handler

--- tutorial.js
+++ tutorial.js
@@ -42,11 +42,51 @@ web.users.list()
     console.dir(channels)
     const firstId = users[0].id;
     chatPayload = {
-      channel: 'D8W2MDBFC',
-      text: 'yo',
-      as_user: true
+      as_user: true,
+      attachments: [
+        {
+          text: 'Attachment 1',
+          "callback_id": "wopr_game",
+          actions: [{
+            "name": "game",
+               "text": "Chess",
+               "type": "button",
+               "value": "chess"
+          }]
+        }
+      ]
     }
     console.dir(chatPayload);
-    return web.chat.postMessage(firstId, 'yo')
+    return web.chat.postMessage('D8W2MDBFC', 'yo', chatPayload)
   })
   .catch(console.error)
+
+  const { createMessageAdapter } = require('@slack/interactive-messages');
+
+  // Initialize adapter using verification token from environment variables
+  const slackMessages = createMessageAdapter(process.env.SLACK_VERIFICATION_TOKEN);
+
+  // Attach action handlers by `callback_id`
+  // (See: https://api.slack.com/docs/interactive-message-field-guide#attachment_fields)
+  slackMessages.action('wopr_game', (payload) => {
+    // `payload` is JSON that describes an interaction with a message.
+    console.log(`The user ${payload.user.name} in team ${payload.team.domain} pressed the welcome button`);
+
+    // The `actions` array contains details about the specific action (button press, menu selection, etc.)
+    const action = payload.actions[0];
+    console.log(`The button had name ${action.name} and value ${action.value}`);
+
+    // You should return a JSON object which describes a message to replace the original.
+    // Note that the payload contains a copy of the original message (`payload.original_message`).
+    const replacement = payload.original_message;
+    // Typically, you want to acknowledge the action and remove the interactive elements from the message
+    replacement.text =`Welcome ${payload.user.name}`;
+    delete replacement.attachments[0].actions;
+    return replacement;
+  });
+
+  // Start the built-in HTTP server
+  const port = process.env.PORT || 3000;
+  slackMessages.start(port).then(() => {
+    console.log(`server listening on port ${port}`);
+  });
